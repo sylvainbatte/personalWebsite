d65aeb7b9ce4caa989ea071ee769b2349ee62a7e
Sylvain <contact@sylvainbatte.com>
2024-06-16 20:36:25 +0200

    update text

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -9,6 +9,7 @@ import projectData from '../datas/project.json';
     <h1 class="title">Mes différents projets</h1>
     <div id="projects">
         <ProjectCardItem name="Portfolio" :description="projectData.descriptions.portfolio" :status="projectData.status.portfolio"/>
+        <ProjectCardItem name="GitHub" :description="projectData.descriptions.github" :status="projectData.status.github"/>
         <ProjectCardItem name="Chronomètre Pomodoro" :description="projectData.descriptions.timer" :status="projectData.status.timer"/>
         <ProjectCardItem name="Utilitaire de texte" :description="projectData.descriptions.textUtil" :status="projectData.status.textUtil"/>
     </div>
